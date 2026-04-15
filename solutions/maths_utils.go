d4a304cab5649f44185e8c6991e77dd193f0746e
zo <zoegough1197@gmail.com>
2026-04-15 15:45:22 +1000

    feat: add solution for problem 1

---
+++ solutions/maths_utils.go
@@ -0,0 +1,5 @@
+package solutions
+
+func triangleNumber(n int) int {
+	return n * (n + 1) / 2
+}
--- solutions/problem_1.go
+++ solutions/problem_1.go
@@ -1,5 +1,38 @@
 package solutions
 
+import "fmt"
+
 func problem1() string {
-	return "TODO"
+	const (
+		max     = 1000
+		factorA = 3
+		factorB = 5
+	)
+
+	cycle := factorA * factorB
+
+	baseMultiples := []int{}
+	baseMultiplesSum := 0
+
+	for m := 1; m <= cycle; m++ {
+		if m%factorA == 0 || m%factorB == 0 {
+			baseMultiples = append(baseMultiples, m)
+			baseMultiplesSum += m
+		}
+	}
+
+	numCycles := max / cycle
+	remainder := max % cycle
+
+	sum := triangleNumber(numCycles-1)*len(baseMultiples)*cycle + baseMultiplesSum*numCycles
+
+	for _, m := range baseMultiples {
+		if m >= remainder {
+			break
+		}
+
+		sum += m + max - remainder
+	}
+
+	return fmt.Sprintf("%d", sum)
 }
